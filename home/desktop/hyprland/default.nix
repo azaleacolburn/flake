6c042b87de428468adf7b8fa1e45207d3a5c6018
Azalea Colburn <azaleacolburn@gmail.com>
2025-01-03 23:22:18 -0800

    re-enable waybar

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -22,7 +22,7 @@ in {
 
     programs = {
       rofi.enable = true;
-      waybar.enable = false;
+      waybar.enable = true;
       hyprlock.enable = true;
       wlogout.enable = true;
     };
@@ -46,7 +46,7 @@ in {
           };
           exec-once = [
             "{ fcitx5 -d -r; fcitx5-remote -r; }&"
-            # (optionals config.programs.waybar.enable "${pkgs.waybar}/bin/waybar&")
+            (optionals config.programs.waybar.enable "${pkgs.waybar}/bin/waybar&")
             (optionals config.services.hypridle.enable "${pkgs.hypridle}/bin/hypridle&")
             (optionals config.services.hyprpaper.enable "${pkgs.hyprpaper}/bin/hyprpaper&")
           ];
