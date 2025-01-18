6a376d42859aaba87de9a8777ae727e202ad607b
Azalea Colburn <azaleacolburn@gmail.com>
2025-01-17 18:13:28 -0800

    remove: old conform config comment

--- nixvim/plugins/conform/default.nix
+++ nixvim/plugins/conform/default.nix
@@ -44,15 +44,6 @@
   plugins.conform-nvim = {
     enable = true;
     settings = {
-      # format_on_save = ''
-      #   function(bufnr)
-      #     -- Disable with a global or buffer-local variable
-      #     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-      #       return
-      #     end
-      #     return { timeout_ms = 500, lsp_format = 'fallback' }
-      #   end
-      # '';
       default_format_opts = {
         lsp_format = "fallback";
         timeout_ms = 1000;
