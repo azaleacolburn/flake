9552712fd25d30e81c1533e0191163c850f27133
Azalea Colburn <azaleacolburn@gmail.com>
2025-07-03 21:21:54 -0700

    add noice to nixvim

--- nixvim/plugins/default.nix
+++ nixvim/plugins/default.nix
@@ -7,11 +7,13 @@
     ./treesitter
     ./cmp
     ./trouble
+    # ./noice
   ];
 
   plugins = {
     web-devicons.enable = true;
     comment.enable = true;
     nvim-autopairs.enable = true;
+    noice.enable = true;
   };
 }
